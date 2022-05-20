a1bac3c43938791b1c8e84234e1fb8b089efc761
o251298 <o251298@gmail.com>
2022-05-20 12:41:38 +0300

    test

---
+++ src/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ctrlv\HelloWorld;
+
+class HelloWorld {
+
+    public static function sayHi() {
+        echo "Hello World" . PHP_EOL;
+    }
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5f48946cd27ae56cf542bb6cfc37d220c1f1d8b3',
+    'name' => 'oleg251298/test',
+  ),
+  'versions' => 
+  array (
+    'oleg251298/test' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5f48946cd27ae56cf542bb6cfc37d220c1f1d8b3',
+    ),
+  ),
+);
